4f3d36e74cd9c4b33f6b872386c8968dc4766136
Gabriel Moreira <gabriel.moreira@psilogroup.com>
2019-11-08 07:49:00 -0300

    boilerplate

--- mandelbrotcl.cpp
+++ mandelbrotcl.cpp
@@ -5,6 +5,7 @@
 #else
 #include <CL/cl.hpp>
 #endif
+#include <SDL.h>
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
@@ -19,12 +20,9 @@ int A[SIZE];
 int B[SIZE];
 int C[SIZE];
 
-
-const char *simple_add_str  = "   void kernel simple_add(global const int* A, global const int* B, global int* C){       "
-            "       C[get_global_id(0)]=A[get_global_id(0)]+B[get_global_id(0)];                 "
-            "   }   ";
-
-
+cl::Context context;
+std::vector<cl::Device> devices;
+cl::Kernel kernel;
 
 void initialize()
 {
@@ -38,19 +36,16 @@ void initialize()
     }
 }
 
-int main(void)
+bool setupOpenCL()
 {
-	cl_int err = CL_SUCCESS;
-	initialize();
-	try {
-
-		std::vector<cl::Platform> platforms;
-        cl_ulong mem_size;
+    cl_int err = CL_SUCCESS;
+    std::vector<cl::Platform> platforms;
+        
 		cl::Platform::get(&platforms);
 		if (platforms.size() == 0) {
 		  
 			std::cout << "Platform size 0\n";
-			return -1;
+			return false;
 		}
 
 		cl_context_properties properties[] =
@@ -60,29 +55,46 @@ int main(void)
 				0
 		};
         
-		cl::Context context(CL_DEVICE_TYPE_ALL, properties);
-
-		std::vector<cl::Device> devices = context.getInfo<CL_CONTEXT_DEVICES>();
 		
-		std::ifstream t("kernel.cl");
-		std::string kernel_str(std::istreambuf_iterator(t),(std::istreambuf_iterator()));
+        context = cl::Context(CL_DEVICE_TYPE_ALL, properties);
+
+		devices = context.getInfo<CL_CONTEXT_DEVICES>();
+        
+        std::ifstream t("kernel.cl");
+		std::string kernel_str(std::istreambuf_iterator<char>(t),(std::istreambuf_iterator<char>()));
 		cl::Program::Sources source(1,
-				std::make_pair( kernel_str.c_str(), kernel_str.lenght()+1));
+				std::make_pair( kernel_str.c_str(), kernel_str.length()+1));
         
 		cl::Program program_ = cl::Program(context, source);
 		program_.build(devices);
 
-		cl::Kernel kernel(program_, "simple_add", &err);
+		kernel = cl::Kernel(program_, "mandelbrot", &err);
+        
+        return true;
+}
+
+int main(void)
+{
+	cl_int err = CL_SUCCESS;
+	initialize();
+	try {
+        
+        setupOpenCL();
+		
+		
+		
 
 		cl::Event event;
-		//cl::Device default_device=devices[0];
 		cl::Buffer buffer_A(context,CL_MEM_READ_WRITE,sizeof(int)*SIZE);
 		cl::Buffer buffer_B(context,CL_MEM_READ_WRITE,sizeof(int)*SIZE);
 		cl::Buffer buffer_C(context,CL_MEM_READ_WRITE,sizeof(int)*SIZE);
+        cl::ImageFormat clImageFormat(CL_RGBA,CL_UNORM_INT8);
+   
+        char *buffer = NULL;
+        cl_int errNum;
+        cl::Image2D imgInput(context,CL_MEM_WRITE_ONLY,clImageFormat,1024,768,0,buffer,&errNum);
 		
-		devices[0].getInfo(CL_DEVICE_LOCAL_MEM_SIZE, &mem_size);
-        std::cout << "Local memory Size: " << (mem_size/1024) << " Kbytes " << std::endl;
-        
+
 		cl::CommandQueue queue(context, devices[0], 0, &err);
 		queue.enqueueWriteBuffer(buffer_A,CL_TRUE,0,sizeof(int)*SIZE,A);
         queue.enqueueWriteBuffer(buffer_B,CL_TRUE,0,sizeof(int)*SIZE,B);
@@ -90,6 +102,7 @@ int main(void)
         kernel.setArg(0,buffer_A);
         kernel.setArg(1,buffer_B);
         kernel.setArg(2,buffer_C);
+        
 		queue.enqueueNDRangeKernel(
 				kernel,
 				cl::NullRange,
@@ -102,10 +115,6 @@ int main(void)
         
         queue.enqueueReadBuffer(buffer_C,CL_TRUE,0,sizeof(int)*SIZE,C);
         
-        for (int i=0;i<SIZE;i++)
-        {
-            int x = C[i];
-        }
 	}
 	catch (cl::Error err) {
 		std::cerr
